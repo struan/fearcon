f1f6d4a7fc8597e6f7aa3202315b8126ed7004d6
Struan Donald <struan@exo.org.uk>
2009-01-01 16:37:12 +0000

    now puts data in and out of datastore according to user

--- main.py
+++ main.py
@@ -19,20 +19,54 @@ import wsgiref.handlers
 import os
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
+from google.appengine.ext import db
+from google.appengine.api import users
+
+class UserLevel(db.Model):
+  user = db.UserProperty()
+  level = db.IntegerProperty()
 
 class MainHandler(webapp.RequestHandler):
 
   def get(self):
+    level = UserLevel()
+    login = ''
+    
+    if users.get_current_user():
+      level = UserLevel.get_by_key_name( users.get_current_user().email() )
+      if not level:
+        level = UserLevel( None, users.get_current_user().email() )
+        level.user = users.get_current_user()
+        level.level = 3
+    else:
+      level.level = 3
+      login = users.create_login_url( '/' )
+        
     template_values = {
-        'levels': [ 5, 4, 3, 2, 1 ],
-        'current': 3
+      'levels': [ 5, 4, 3, 2, 1 ],
+      'current': level.level,
+      'login': login
     }
     path = os.path.join(os.path.dirname(__file__), 'tmpl/index.html')
     self.response.out.write(template.render(path, template_values))
 
+class ChangeLevel(webapp.RequestHandler):
+  def post(self):
+    if users.get_current_user():
+      level = UserLevel.get_by_key_name( users.get_current_user().email() )
+      
+      if not level:
+        level = UserLevel( None, users.get_current_user().email() )
+        level.user = users.get_current_user()
+        
+      level.level = int( self.request.get('level') )
+      level.put()
+    self.response.out.write('OK')
+    
 
 def main():
-  application = webapp.WSGIApplication([('/', MainHandler)],
+  application = webapp.WSGIApplication([('/', MainHandler),
+                                        ('/update', ChangeLevel)],
                                        debug=True)
   wsgiref.handlers.CGIHandler().run(application)
 
