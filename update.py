5fbcec86bd2693aa72850515790901f7507829fa
Struan Donald <struan@exo.org.uk>
2009-01-02 13:38:29 +0000

    more changes required for two request update

--- update.py
+++ update.py
@@ -7,37 +7,49 @@ from google.appengine.ext.webapp import template
 from google.appengine.api import users
 from fearcon.db import *
 
-class ChangeLevel(webapp.RequestHandler):
+class ChangeUserLevel(webapp.RequestHandler):
   def post(self):
     if users.get_current_user():
       level = UserLevel.get_by_key_name( users.get_current_user().email() )
       new_level = int( self.request.get('level') )
-      avgs = AverageLevel.all()
-      
-      if avgs.count() > 0:
-        avg = avgs[0]
-      else:
-        avg = AverageLevel()
-        avg.total = 0
-        avg.count = 0
       
       if not level:
         level = UserLevel( None, users.get_current_user().email() )
         level.user = users.get_current_user()
-        level.nick = level.user.nickname()
-        avg.count += 1
-        avg.total += new_level
-      else:
-        avg.total -= level.level
-        avg.total += new_level
+        level.nick = level.user.nickname(
         
       level.level = new_level
       level.put()
+    self.response.out.write('OK')
+    
+class ChangeTotalLevel(webapp.RequestHandler):
+  def post(self):
+      old_level = int( self.request.get('old_level') )
+      new_level = int( self.request.get('new_level') )
+      avgs = db.Query(AverageLevel)
+
+      if avgs:
+        avg = avgs.get()
+      else:
+        avg = AverageLevel()
+        avg.total = 0
+        avg.count = 0
+        
+      if old_level == 0:
+        avg.count += 1
+        avg.total += new_level
+      else:
+        total = avg.total
+        total -= old_level
+        total += new_level
+        avg.total = total
+
       avg.put()
     self.response.out.write('OK')
 
 def main():
-  application = webapp.WSGIApplication([('/update', ChangeLevel)],
+  application = webapp.WSGIApplication([('/update_user', ChangeUserLevel),
+                                        ('/update_total', ChangeTotalLevel)],
                                        debug=True)
   wsgiref.handlers.CGIHandler().run(application)
 
