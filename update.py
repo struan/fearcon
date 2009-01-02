1752f8560de2a28e0259f8e038acb40731dfdd64
Struan Donald <struan@exo.org.uk>
2009-01-02 14:14:19 +0000

    fix syntax errors/typos :(

--- update.py
+++ update.py
@@ -16,7 +16,7 @@ class ChangeUserLevel(webapp.RequestHandler):
       if not level:
         level = UserLevel( None, users.get_current_user().email() )
         level.user = users.get_current_user()
-        level.nick = level.user.nickname(
+        level.nick = level.user.nickname()
         
       level.level = new_level
       level.put()
@@ -24,27 +24,27 @@ class ChangeUserLevel(webapp.RequestHandler):
     
 class ChangeTotalLevel(webapp.RequestHandler):
   def post(self):
-      old_level = int( self.request.get('old_level') )
-      new_level = int( self.request.get('new_level') )
-      avgs = db.Query(AverageLevel)
+    old_level = int( self.request.get('old_level') )
+    new_level = int( self.request.get('new_level') )
+    avgs = db.Query(AverageLevel)
 
-      if avgs:
-        avg = avgs.get()
-      else:
-        avg = AverageLevel()
-        avg.total = 0
-        avg.count = 0
-        
-      if old_level == 0:
-        avg.count += 1
-        avg.total += new_level
-      else:
-        total = avg.total
-        total -= old_level
-        total += new_level
-        avg.total = total
+    if avgs:
+      avg = avgs.get()
+    else:
+      avg = AverageLevel()
+      avg.total = 0
+      avg.count = 0
+      
+    if old_level == 0:
+      avg.count += 1
+      avg.total += new_level
+    else:
+      total = avg.total
+      total -= old_level
+      total += new_level
+      avg.total = total
 
-      avg.put()
+    avg.put()
     self.response.out.write('OK')
 
 def main():
